956c5a87bff9bee22eab561918bdc1b831c72171
Ben Fornefeld <ben.fornefeld@gmail.com>
2022-01-02 22:05:55 +0100

    localization + colormode + layouts

---
+++ components/layout/main.js
@@ -0,0 +1,26 @@
+import Head from "next/head"
+import { Box, Container } from "@chakra-ui/react"
+
+const Layout = ({ children }) => {
+    return (
+        <Box as="main">
+            <Head>   {/*TODO: HEAD META */}
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Description" />
+                <title>Title</title>
+            </Head>
+
+            {/* TODO: NAVBAR */}
+
+            <Container maxW="container.md" m={0}>
+
+                {children}
+
+                {/* TODO: FOOTER */}
+
+            </Container>
+        </Box>
+    )
+}
+
+export default Layout
