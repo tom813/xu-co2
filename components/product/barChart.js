a8b3fbf0cdc09aa297500a8c53d45bb732e891f4
tom813 <tom.engelmann@outlook.de>
2022-01-28 22:11:58 +0100

    changed bar chart color

--- components/product/barChart.js
+++ components/product/barChart.js
@@ -12,7 +12,7 @@ function BarChart({percantage}){
                 borderRadius="50px"
                 overflow="hidden"
             >
-                <Box w={`${percantage}%`} h="inherit" bg={useColorModeValue("#805ad5", "#fdb38d")} zIndex="-1">
+                <Box w={`${percantage}%`} h="inherit" bg={useColorModeValue("#805ad5", "#fbd38d")} zIndex="-1">
 
                 </Box>
                 <Box position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" fontWeight="600"
