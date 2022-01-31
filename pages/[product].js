5383b6cd7bff5ad4c27806757aa3d33b6d6126c4
Ben Fornefeld <ben.fornefeld@gmail.com>
2022-01-31 18:28:47 +0100

    finalized project

--- pages/[product].js
+++ pages/[product].js
@@ -1,183 +1,212 @@
-import {useRouter} from 'next/router'
-import dynamic from 'next/dynamic'
-import { Box, Flex, Heading, Image, Text, Button, useColorModeValue,  useToast, StatGroup, Stat, StatLabel, StatNumber } from '@chakra-ui/react'
-const Tabs = dynamic(import('@chakra-ui/react').then(mod => mod.Tabs), {ssr: false})
-import { TabList, Tab, TabPanel, TabPanels} from '@chakra-ui/react'
-import DataExample from '../components/product/dataExample'
-import SubsetTable from '../components/product/subsetTable'
-import BarChart from '../components/product/barChart'
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import {
+  Box,
+  Flex,
+  Heading,
+  Image,
+  Text,
+  Button,
+  useColorModeValue,
+  useToast,
+  StatGroup,
+  Stat,
+  StatLabel,
+  StatNumber,
+} from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+const Tabs = dynamic(
+  import("@chakra-ui/react").then((mod) => mod.Tabs),
+  { ssr: false }
+);
+import { TabList, Tab, TabPanel, TabPanels } from "@chakra-ui/react";
+import DataExample from "../components/product/dataExample";
+import SubsetTable from "../components/product/subsetTable";
+import BarChart from "../components/product/barChart";
 
-const tableSampleData = [
+const ProductPage = () => {
+  const router = useRouter();
 
-]
-const sampleStateSubset = [
+  const [props, setProps] = useState(null);
+
+  const toast = useToast();
+  const coverages = [
     {
-        id: 1, 
-        subsetOf: 'dentist',
-        state: 'Berlin',
-        num_of_entries: 123,
-        price: 300
-    }
-]
+      name: "Adress",
+      percantage: props ? props.branch[0].adress_coverage : null,
+    },
+    {
+      name: "Phone",
+      percantage: props ? props.branch[0].phone_coverage : null,
+    },
+    {
+      name: "Website",
+      percantage: props ? props.branch[0].website_coverage : null,
+    },
+    {
+      name: "Email",
+      percantage: props ? props.branch[0].email_coverage : null,
+    },
+  ];
 
-export async function getStaticPaths(){
-    const res = await fetch('http://localhost:3000/api/allbranches')
-    const data = await res.json()
+  async function fetchData() {
+    const baseUrl = `${document.location.protocol}//${document.location.host}`;
 
-    var paths = []
-    data.map((sample) => (
-        paths.push({params: {product: `${sample.name}`}})
-    ))
+    const stateSubsets = await await (
+      await fetch(`${baseUrl}/api/stateSubsets`)
+    ).json();
 
-    return{
-        paths: paths,
-        fallback: false
-    }
-}
+    const branch = await (
+      await fetch(`${baseUrl}/api/branch/${router.query.product}`)
+    ).json();
 
-export async function getStaticProps(context){
-    var stateSubsets = await fetch('http://localhost:3000/api/stateSubsets')
-    stateSubsets = await stateSubsets.json()
-    var branch = await fetch(`http://localhost:3000/api/branch/${context.params.product}`)
-    branch = await branch.json()
-    return{
-        props: {
-                productData: context.params, 
-                stateSubsets: stateSubsets,
-                branch: branch
-            }
-    }
+    setProps({
+      branch: branch,
+      stateSubsets: stateSubsets,
+    });
+  }
 
-}
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  const btnColorScheme = useColorModeValue("purple", "orange");
 
-const ProductPage = ({productData, stateSubsets, branch}) => {
-    const toast = useToast()
-    const coverages = [
-        {name: 'Adress', percantage: branch[0].adress_coverage},
-        {name: 'Phone', percantage: branch[0].phone_coverage},
-        {name: 'Website', percantage: branch[0].website_coverage},
-        {name: 'Email', percantage: branch[0].email_coverage},
-    ]
-    return (
-        <Box w="100%" maxW="1400px" margin="0 auto" padding="0 2%">
-            <Flex justifyContent="space-evenly" alignItems="center" gap="10%" wrap="wrap">
-                <Box w="300px" height="300px" bgSize="cover" bgPosition="center" bgImage="/images/icons/fav-folder-dynamic-color.png" />
-                <Flex flexDir="column" alignItems="stretch" justifyContent="space-between" gap="10px">
-                    <Heading as="h1">
-                        Dataset of {productData.product}s
-                    </Heading>
-                    
-                    <StatGroup m="1rem 0">
-                    <Stat>
-                        <StatLabel>Price</StatLabel>
-                        <StatNumber>
-                            €{branch[0].price}
-                        </StatNumber>
-                        
-                    </Stat>
+  return (
+    <Box w="100%" maxW="1400px" margin="0 auto" padding="0 2%">
+      {props ? (
+        <>
+          <Flex
+            justifyContent="space-evenly"
+            alignItems="center"
+            gap="10%"
+            wrap="wrap"
+          >
+            <Box
+              w="300px"
+              height="300px"
+              bgSize="cover"
+              bgPosition="center"
+              bgImage="/images/icons/fav-folder-dynamic-color.png"
+            />
+            <Flex
+              flexDir="column"
+              alignItems="stretch"
+              justifyContent="space-between"
+              gap="10px"
+            >
+              <Heading as="h1">Dataset of {props.branch[0].name}s</Heading>
 
-                    <Stat>
-                        <StatLabel>Number of entries</StatLabel>
-                        <StatNumber>
-                            {branch[0].num_of_entries}
-                        </StatNumber>
-                        
-                    </Stat>
-                    </StatGroup>
-                    <Flex flexDir="column" gap="10px">
-                        <Button colorScheme={useColorModeValue("purple", "orange")}>buy</Button>
-                        <Button 
-                            as="a"
-                            href="/downloads/example.csv"
-                            download
-                            colorScheme={useColorModeValue("purple", "orange")}
-                            onClick={() =>
-                                toast({
-                                  title: 'Download successfull.',
-                                  status: 'success',
-                                  duration: 2000,
-                                  isClosable: true,
-                                })
-                              }
-                        >download example</Button>
-                    </Flex>
-                    
-                </Flex>
+              <StatGroup m="1rem 0">
+                <Stat>
+                  <StatLabel>Price</StatLabel>
+                  <StatNumber>€{props.branch[0].price}</StatNumber>
+                </Stat>
+
+                <Stat>
+                  <StatLabel>Number of entries</StatLabel>
+                  <StatNumber>{props.branch[0].num_of_entries}</StatNumber>
+                </Stat>
+              </StatGroup>
+              <Flex flexDir="column" gap="10px">
+                <Button colorScheme={btnColorScheme}>buy</Button>
+                <Button
+                  as="a"
+                  href="/downloads/example.csv"
+                  download
+                  colorScheme={btnColorScheme}
+                  onClick={() =>
+                    toast({
+                      title: "Download successfull.",
+                      status: "success",
+                      duration: 2000,
+                      isClosable: true,
+                    })
+                  }
+                >
+                  download example
+                </Button>
+              </Flex>
             </Flex>
-            
-            <Box m="50px 0">
-                <Heading mb="1rem">Example Data</Heading>
-                <DataExample Code={{
-                    Name: "Mustermann Limited",
-                    Branch: "Dentist",
-                    Adress: "Hauptstraße 42",
-                    City: "Coruscant",
-                    Phone: "0176 662607015",
-                    Website: "www.localhost.com",
-                    Email: "hmu@local.host"
-                }} />
+          </Flex>
 
-                
-            </Box>
+          <Box m="50px 0">
+            <Heading mb="1rem">Example Data</Heading>
+            <DataExample
+              Code={{
+                Name: "Mustermann Limited",
+                Branch: "Dentist",
+                Adress: "Hauptstraße 42",
+                City: "Coruscant",
+                Phone: "0176 662607015",
+                Website: "www.localhost.com",
+                Email: "hmu@local.host",
+              }}
+            />
+          </Box>
 
-            <Box>
-                <Heading as="h2">
-                    Data coverage
-                </Heading>
-                {coverages.map((coverage) => {
-                    return(
-                        <Box key={coverage.name} m="2rem 0">
-                            <Heading as="h4" fontSize="1.4rem" fontWeight="300" mb=".5rem">
-                                {coverage.name} coverage
-                            </Heading>
-                            <BarChart percantage={coverage.percantage} />
-                        </Box>
-                    )
-                })}
-                
-            </Box>
+          <Box>
+            <Heading as="h2">Data coverage</Heading>
+            {coverages.map((coverage) => {
+              return (
+                <Box key={coverage.name} m="2rem 0">
+                  <Heading
+                    as="h4"
+                    fontSize="1.4rem"
+                    fontWeight="300"
+                    mb=".5rem"
+                  >
+                    {coverage.name} coverage
+                  </Heading>
+                  <BarChart percantage={coverage.percantage} />
+                </Box>
+              );
+            })}
+          </Box>
 
-            <Box>
-                <Heading mb="1rem">State subsets</Heading>
-                <SubsetTable tableData={stateSubsets} />
-            </Box>
+          <Box>
+            <Heading mb="1rem">State subsets</Heading>
+            <SubsetTable tableData={props.stateSubsets} />
+          </Box>
 
-            
-            
-            <Box m="50px 0">
-                <Heading as="h2" mb="1rem">Legal information</Heading>
-                <Tabs variant='soft-rounded' colorScheme={useColorModeValue("purple", "orange")}>
-                    <TabList>
-                        <Tab>Source of Data</Tab>
-                        <Tab>Data Quality</Tab>
-                        <Tab>Privacy</Tab>
-                        <Tab></Tab>
-                    </TabList>
-                    <TabPanels>
-                        <TabPanel>
-                        <p>
-                            Here you can find information about the source of Data.
-                            <br /><br />
-                            This will contain information about the websites and the rules our scrapers are going through.
-                            <br /><br />
-                            If the data is available you'll find all information here.
-                        </p>
-                        </TabPanel>
-                        <TabPanel>
-                        <p>At the moment no data is available</p>
-                        </TabPanel>
-                        <TabPanel>
-                            <p>
-                                At the moment no data is available
-                            </p>
-                        </TabPanel>
-                    </TabPanels>
-                </Tabs>
-            </Box>
-            
-        </Box>
-    )
-}
+          <Box m="50px 0">
+            <Heading as="h2" mb="1rem">
+              Legal information
+            </Heading>
+            <Tabs variant="soft-rounded" colorScheme={btnColorScheme}>
+              <TabList>
+                <Tab>Source of Data</Tab>
+                <Tab>Data Quality</Tab>
+                <Tab>Privacy</Tab>
+                <Tab></Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <p>
+                    Here you can find information about the source of Data.
+                    <br />
+                    <br />
+                    This will contain information about the websites and the
+                    rules our scrapers are going through.
+                    <br />
+                    <br />
+                    If the data is available you&apos;ll find all information
+                    here.
+                  </p>
+                </TabPanel>
+                <TabPanel>
+                  <p>At the moment no data is available</p>
+                </TabPanel>
+                <TabPanel>
+                  <p>At the moment no data is available</p>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Box>
+        </>
+      ) : null}
+    </Box>
+  );
+};
 
-export default ProductPage
+export default ProductPage;
--- pages/overview/index.js
+++ pages/overview/index.js
@@ -1,22 +1,34 @@
-import {Image, Flex, Box, Text, Heading, Center, SimpleGrid, Link} from '@chakra-ui/react';
-import Card from '../../components/overview/card';
-import Layout from '../../components/layout/article';
-import Section from '../../components/global/section';
-import BarChart from '../../components/product/barChart';
+import {
+  Image,
+  Flex,
+  Box,
+  Text,
+  Heading,
+  Center,
+  SimpleGrid,
+  Link,
+} from "@chakra-ui/react";
+import Card from "../../components/overview/card";
+import Layout from "../../components/layout/article";
+import Section from "../../components/global/section";
+import BarChart from "../../components/product/barChart";
+import { useEffect, useState } from "react";
 
+const Overview = () => {
+  const [branches, setBranches] = useState(null);
 
-export async function getStaticProps(){
-  var branches = await fetch(`http://localhost:3000/api/allbranches`)
-  branches = await branches.json()
-  return{
-      props: {
-              branches: branches
-          }
+  async function fetchData() {
+    const baseUrl = `${document.location.protocol}//${document.location.host}`;
+
+    const branches = await (await fetch(`${baseUrl}/api/allbranches`)).json();
+
+    setBranches(branches);
   }
 
-}
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-const Overview = ({branches}) => {
   /* let cards = [];
   for(let i = 0; i < 6; i++){
     cards.push(<Card key={i} icon={'/images/icons/sheild-front-color.png'} title={'Lorem ipsum'} description={'lLorem ipsum dolor sit amet consectetur adipiLorem ipsum dolor sit amet consectetur adipiorem'}/>);
@@ -24,46 +36,78 @@ const Overview = ({branches}) => {
 
   return (
     <Layout>
-      <Section>
-        <Heading>Branche overview</Heading>
-        <Flex flexDirection={'row'}>
-          <Box flex={2}>
+      {branches ? (
+        <>
+          <Section>
+            <Heading>Branche overview</Heading>
+            <Flex flexDirection={"row"}>
+              <Box flex={2}>
+                <Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Officia molestiae quibusdam esse eaque impedit a! Enim
+                  reprehenderit debitis neque ullam! Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Eligendi quibusdam suscipit
+                  voluptatum accusamus aperiam quisquam error labore aspernatur
+                  nulla numquam?
+                </Text>
+              </Box>
+              <Box flex={1}>
+                <Image src="/images/icons/book-color.png"></Image>
+              </Box>
+            </Flex>
+          </Section>
+          <Section>
+            <Center mb={8}>
+              <Heading>Overview</Heading>
+            </Center>
+
+            <SimpleGrid columns={3} spacing={5} mb="5em">
+              {branches.map((branch) => {
+                return (
+                  <Link
+                    key={branch.name}
+                    href={`/${branch.name}`}
+                    textDecoration="none"
+                    _hover={{ textDecoration: "none", transform: "scale(1.1)" }}
+                  >
+                    <Card branchData={branch} textDecoration="none" />
+                  </Link>
+                );
+              })}
+            </SimpleGrid>
+          </Section>
+          <Section>
             <Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia molestiae quibusdam esse eaque impedit a! Enim reprehenderit debitis neque ullam!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quibusdam suscipit voluptatum accusamus aperiam quisquam error labore aspernatur nulla numquam?
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Molestias totam iure illum nemo assumenda. Non nemo fugiat id, ab
+              at, enim illum temporibus in incidunt maxime repellendus neque
+              quidem minima inventore quisquam voluptate. Odit repellat
+              repellendus possimus quidem accusamus porro, voluptatum dolore
+              quisquam ab, labore neque obcaecati doloribus laboriosam commodi
+              alias tempora quos tempore reprehenderit necessitatibus dolorem
+              quis earum atque minus! Ea magnam, perferendis voluptatem dolorum
+              ipsa maxime. Dolorum totam optio architecto numquam tempore fuga
+              autem libero at enim ipsum. Est nobis excepturi earum iusto
+              voluptatem architecto quasi facilis possimus quis nam esse aperiam
+              ipsa, cum numquam distinctio labore mollitia iste? Voluptates enim
+              perferendis aliquid, optio minus ipsam saepe quo maiores
+              consequuntur aliquam omnis vel odio ex odit minima cumque
+              repellendus hic quidem neque corporis ullam molestiae tempora.
+              Cupiditate consectetur libero ipsa consequuntur necessitatibus
+              voluptatem eos et quaerat rem vero! Ab numquam sit magnam quod
+              quaerat itaque consectetur recusandae autem molestias incidunt
+              deserunt cupiditate totam odio repellendus odit consequuntur
+              asperiores nulla amet nihil quos perspiciatis, facilis nostrum
+              tenetur quibusdam? Sed, illum voluptatum et architecto ex cum
+              commodi aliquid quibusdam, libero incidunt saepe sunt doloribus
+              provident sit culpa est. Dicta distinctio accusamus hic qui maxime
+              laborum. Eius deleniti quisquam voluptatum sapiente.
             </Text>
-          </Box>
-          <Box flex={1}>
-            <Image src="/images/icons/book-color.png"></Image>
-          </Box>
-        </Flex>
-      </Section>
-
-      <Section>
-        <Center mb={8}>
-          <Heading>Overview</Heading>
-        </Center>
-
-        
-        <SimpleGrid columns={3} spacing={5} mb='5em'>
-          
-          {branches.map((branch) => {
-            return(
-              <Link key={branch.name} href={`/${branch.name}`} textDecoration="none" _hover={{textDecoration: 'none', transform: 'scale(1.1)'}}>
-                <Card branchData={branch} textDecoration="none" />
-              </Link>
-            )
-          })}
-        </SimpleGrid>
-      </Section>
-
-      <Section>
-        <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias totam iure illum nemo assumenda. Non nemo fugiat id, ab at, enim illum temporibus in incidunt maxime repellendus neque quidem minima inventore quisquam voluptate. Odit repellat repellendus possimus quidem accusamus porro, voluptatum dolore quisquam ab, labore neque obcaecati doloribus laboriosam commodi alias tempora quos tempore reprehenderit necessitatibus dolorem quis earum atque minus! Ea magnam, perferendis voluptatem dolorum ipsa maxime. Dolorum totam optio architecto numquam tempore fuga autem libero at enim ipsum. Est nobis excepturi earum iusto voluptatem architecto quasi facilis possimus quis nam esse aperiam ipsa, cum numquam distinctio labore mollitia iste? Voluptates enim perferendis aliquid, optio minus ipsam saepe quo maiores consequuntur aliquam omnis vel odio ex odit minima cumque repellendus hic quidem neque corporis ullam molestiae tempora. Cupiditate consectetur libero ipsa consequuntur necessitatibus voluptatem eos et quaerat rem vero! Ab numquam sit magnam quod quaerat itaque consectetur recusandae autem molestias incidunt deserunt cupiditate totam odio repellendus odit consequuntur asperiores nulla amet nihil quos perspiciatis, facilis nostrum tenetur quibusdam? Sed, illum voluptatum et architecto ex cum commodi aliquid quibusdam, libero incidunt saepe sunt doloribus provident sit culpa est. Dicta distinctio accusamus hic qui maxime laborum. Eius deleniti quisquam voluptatum sapiente.</Text>
-      </Section>
-
-      
+          </Section>
+        </>
+      ) : null}
     </Layout>
   );
-}
+};
 
-export default Overview
+export default Overview;
